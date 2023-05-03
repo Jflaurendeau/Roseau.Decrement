92a2f1a46376ed78f3c22f8ad69f5ef3cbafc4bf
Jean-François Laurendeau <laurendeau.jf@outlook.com>
2023-05-02 21:16:06 -0400

    Adjustement to allocated less memory and correcting hashcode for GenderedUnisex decrement.

--- src/Roseau.Decrement/Aggregates/Decrements/LifeTables/GenderedDecrementT.cs
+++ src/Roseau.Decrement/Aggregates/Decrements/LifeTables/GenderedDecrementT.cs
@@ -9,83 +9,81 @@ namespace Roseau.Decrement.Aggregates.Decrements.LifeTables;
 public class GenderedDecrement<TGenderedIndividual> : Decrement<TGenderedIndividual>, IUnisexDecrementT<TGenderedIndividual>
 	where TGenderedIndividual : IGenderedIndividual
 {
+	#region Constructors
 	public GenderedDecrement(IDecrementTable<TGenderedIndividual> table) : base(table, default!, default!, default!) { }
 	public GenderedDecrement(IDecrementTable<TGenderedIndividual> table, IImprovement<TGenderedIndividual> improvementScale) : base(table, improvementScale, default!, default!) { }
 	public GenderedDecrement(IDecrementTable<TGenderedIndividual> table, IAdjustment<TGenderedIndividual> adjustment) : base(table, default!, adjustment, default!) { }
 	public GenderedDecrement(IDecrementTable<TGenderedIndividual> table, IImprovement<TGenderedIndividual> improvementScale, IAdjustment<TGenderedIndividual> adjustment) : base(table, improvementScale, adjustment, default!) { }
 	public GenderedDecrement(IDecrementTable<TGenderedIndividual> table, IImprovement<TGenderedIndividual> improvementScale, IAdjustment<TGenderedIndividual> adjustment, IMemoryCache memoryCache) : base(table, improvementScale, adjustment, memoryCache) { }
+	#endregion
 
-	protected delegate decimal DecrementOrSurvivalUnisexProbabilityIn(TGenderedIndividual individual, in DateOnly calculationDate, in DateOnly decrementDate, in decimal manProportion);
-	protected int GetHashCode(TGenderedIndividual individual, in DateOnly calculationDate, OrderedDates dates, DecrementOrSurvivalUnisexProbabilityIn decrementOrSurvivalProbability, decimal manProportion) => HashCode.Combine(_Table, individual.Gender, individual.DateOfBirth, calculationDate, dates, decrementOrSurvivalProbability, manProportion);
+	private delegate decimal DecrementOrSurvivalUnisexProbabilityIn(TGenderedIndividual manIndividual, TGenderedIndividual womanIndividual, in DateOnly calculationDate, in DateOnly decrementDate, in decimal manProportion);
 
 	#region Private methods
-	private decimal SurvivalUnisexBetweenIntegerAge(TGenderedIndividual individual, in DateOnly firstDate, in DateOnly secondDate, in decimal manProportion)
+	private int GetHashCode(TGenderedIndividual individual, in DateOnly calculationDate, OrderedDates dates, DecrementOrSurvivalUnisexProbabilityIn decrementOrSurvivalProbability, in decimal manProportion) => HashCode.Combine(_Table, individual.DateOfBirth, calculationDate, dates, decrementOrSurvivalProbability, manProportion);
+	private decimal SurvivalUnisexBetweenIntegerAge(TGenderedIndividual manIndividual, TGenderedIndividual womanIndividual, in DateOnly firstDate, in DateOnly secondDate, in decimal manProportion)
 	{
-		TGenderedIndividual manIndividual = individual.Gender == Gender.Man ? individual : (TGenderedIndividual)individual.AsOtherGender();
-		TGenderedIndividual womanIndividual = individual.Gender == Gender.Woman ? individual : (TGenderedIndividual)individual.AsOtherGender();
 		decimal manDecrement = DecrementRate(manIndividual, firstDate);
 		decimal womanDecrement = DecrementRate(womanIndividual, firstDate);
 		return 1 - IDecrement.UniformDecrementDistribution(manDecrement * manProportion + (1 - manProportion) * womanDecrement, firstDate, secondDate);
 	}
-	private decimal YearlyUnisexSurvival(TGenderedIndividual individual, in DateOnly decrementDate, in decimal manProportion)
+	private decimal YearlyUnisexSurvival(TGenderedIndividual manIndividual, TGenderedIndividual womanIndividual, in DateOnly decrementDate, in decimal manProportion)
 	{
-		TGenderedIndividual manIndividual = individual.Gender == Gender.Man ? individual : (TGenderedIndividual)individual.AsOtherGender();
-		TGenderedIndividual womanIndividual = individual.Gender == Gender.Woman ? individual : (TGenderedIndividual)individual.AsOtherGender();
 		decimal manDecrement = DecrementRate(manIndividual, decrementDate);
 		decimal womanDecrement = DecrementRate(womanIndividual, decrementDate);
 		return 1 - manDecrement * manProportion + (1 - manProportion) * womanDecrement;
 	}
-	private decimal GetSurvivalUnisexProbability(TGenderedIndividual individual, in DateOnly calculationDate, in DateOnly decrementDate, in decimal manProportion)
+	private decimal GetSurvivalUnisexProbability(TGenderedIndividual manIndividual, TGenderedIndividual womanIndividual, in DateOnly calculationDate, in DateOnly decrementDate, in decimal manProportion)
 	{
 		int decrementYear = decrementDate.Year;
 		if (calculationDate.Year == decrementYear)
-			return SurvivalUnisexBetweenIntegerAge(individual, calculationDate, decrementDate, manProportion);
+			return SurvivalUnisexBetweenIntegerAge(manIndividual, womanIndividual, calculationDate, decrementDate, manProportion);
 
-		decimal survivalProbability = SurvivalUnisexBetweenIntegerAge(individual, calculationDate, decrementDate, manProportion);
+		decimal survivalProbability = SurvivalUnisexBetweenIntegerAge(manIndividual, womanIndividual, calculationDate, decrementDate, manProportion);
 		DateOnly nextDate = calculationDate.FirstDayOfFollowingYear();
 		while (nextDate.Year < decrementYear)
 		{
-			survivalProbability *= YearlyUnisexSurvival(individual, nextDate, manProportion);
+			survivalProbability *= YearlyUnisexSurvival(manIndividual, womanIndividual, nextDate, manProportion);
 			nextDate = nextDate.AddYears(1);
 		}
-		survivalProbability *= SurvivalUnisexBetweenIntegerAge(individual, nextDate, decrementDate, manProportion);
+		survivalProbability *= SurvivalUnisexBetweenIntegerAge(manIndividual, womanIndividual, nextDate, decrementDate, manProportion);
 		return survivalProbability;
 	}
-	private decimal GetDecrementUnisexProbability(TGenderedIndividual individual, in DateOnly calculationDate, in DateOnly decrementDate, in decimal manProportion) => 1 - GetSurvivalUnisexProbability(individual, calculationDate, decrementDate, manProportion);
-	private decimal GetUnisexProbability(TGenderedIndividual individual, in DateOnly calculationDate, in DateOnly decrementDate, DecrementOrSurvivalUnisexProbabilityIn decrementOrSurvivalProbability, in decimal manProportion)
+	private decimal GetDecrementUnisexProbability(TGenderedIndividual manIndividual, TGenderedIndividual womanIndividual, in DateOnly calculationDate, in DateOnly decrementDate, in decimal manProportion) => 1 - GetSurvivalUnisexProbability(manIndividual, womanIndividual, calculationDate, decrementDate, manProportion);
+	private decimal GetUnisexProbability(TGenderedIndividual manIndividual, TGenderedIndividual womanIndividual, in DateOnly calculationDate, in DateOnly decrementDate, DecrementOrSurvivalUnisexProbabilityIn decrementOrSurvivalProbability, in decimal manProportion)
 	{
 		if (calculationDate > decrementDate) throw new ArgumentException($"{nameof(calculationDate)} must be before {nameof(decrementDate)}");
-		if (individual.DateOfBirth > calculationDate) throw new ArgumentException($"{nameof(individual.DateOfBirth)} must be before {nameof(calculationDate)}");
-		if (!_Table.IsOlderThanLastAgeOfTheTable(individual, decrementDate))
-			return decrementOrSurvivalProbability(individual, calculationDate, decrementDate, manProportion);
+		if (manIndividual.DateOfBirth > calculationDate) throw new ArgumentException($"{nameof(manIndividual.DateOfBirth)} must be before {nameof(calculationDate)}");
+		if (!_Table.IsOlderThanLastAgeOfTheTable(manIndividual, decrementDate))
+			return decrementOrSurvivalProbability(manIndividual, womanIndividual, calculationDate, decrementDate, manProportion);
 		if (decrementOrSurvivalProbability == GetSurvivalUnisexProbability)
 			return 0m;
 		return 1m;
 	}
-	private decimal[] GetUnisexProbabilities(TGenderedIndividual individual, in DateOnly calculationDate, OrderedDates dates, DecrementOrSurvivalUnisexProbabilityIn decrementOrSurvivalProbability, in decimal manProportion)
+	private decimal[] GetUnisexProbabilities(TGenderedIndividual manIndividual, TGenderedIndividual womanIndividual, in DateOnly calculationDate, OrderedDates dates, DecrementOrSurvivalUnisexProbabilityIn decrementOrSurvivalProbability, in decimal manProportion)
 	{
-		int key = GetHashCode(individual, calculationDate, dates, decrementOrSurvivalProbability, manProportion);
+		int key = GetHashCode(manIndividual, calculationDate, dates, decrementOrSurvivalProbability, manProportion);
 		if (!_Cache.TryGetValue(key, out decimal[]? result))
 		{
 			if (calculationDate > dates[0]) throw new ArgumentException($"{nameof(calculationDate)} must be before the first date of {nameof(dates)}");
-			if (individual.DateOfBirth > calculationDate) throw new ArgumentException($"{nameof(individual.DateOfBirth)} must be before {nameof(calculationDate)}");
-			result = UnisexProbabilities(individual, in calculationDate, dates, decrementOrSurvivalProbability, manProportion);
+			if (manIndividual.DateOfBirth > calculationDate) throw new ArgumentException($"{nameof(manIndividual.DateOfBirth)} must be before {nameof(calculationDate)}");
+			result = UnisexProbabilities(manIndividual, womanIndividual, in calculationDate, dates, decrementOrSurvivalProbability, manProportion);
 			_Cache.Set(key, result, GetMemoryCacheEntryOptions());
 		}
 		return result!;
 	}
-	private decimal[] UnisexProbabilities(TGenderedIndividual individual, in DateOnly calculationDate, OrderedDates dates, DecrementOrSurvivalUnisexProbabilityIn decrementOrSurvivalProbability, in decimal manProportion)
+	private decimal[] UnisexProbabilities(TGenderedIndividual manIndividual, TGenderedIndividual womanIndividual, in DateOnly calculationDate, OrderedDates dates, DecrementOrSurvivalUnisexProbabilityIn decrementOrSurvivalProbability, in decimal manProportion)
 	{
 		int length = dates.Count;
 		decimal[] probabilities = new decimal[length];
-		DateOnly lastPossibleDecrementDate = _Table.LastPossibleDecrementDate(individual);
+		DateOnly lastPossibleDecrementDate = _Table.LastPossibleDecrementDate(manIndividual);
 		int i = 0;
-		probabilities[i] = decrementOrSurvivalProbability(individual, in calculationDate, dates[i], manProportion);
+		probabilities[i] = decrementOrSurvivalProbability(manIndividual, womanIndividual, in calculationDate, dates[i], manProportion);
 		i++;
 		while (i < length
 			&& dates[i] < lastPossibleDecrementDate)
 		{
-			probabilities[i] = probabilities[i - 1] * decrementOrSurvivalProbability(individual, dates[i - 1], dates[i], manProportion);
+			probabilities[i] = probabilities[i - 1] * decrementOrSurvivalProbability(manIndividual, womanIndividual, dates[i - 1], dates[i], manProportion);
 			i++;
 		}
 		while (i < length)
@@ -106,7 +104,9 @@ public class GenderedDecrement<TGenderedIndividual> : Decrement<TGenderedIndivid
 			return GetProbabilities(individual.Gender == Gender.Man ? individual : (TGenderedIndividual)individual.AsOtherGender(), calculationDate, dates, GetDecrementProbability);
 		if (manProportion == Decimal.Zero)
 			return GetProbabilities(individual.Gender == Gender.Woman ? individual : (TGenderedIndividual)individual.AsOtherGender(), calculationDate, dates, GetDecrementProbability);
-		return GetUnisexProbabilities(individual, calculationDate, dates, GetDecrementUnisexProbability, manProportion);
+		TGenderedIndividual manIndividual = individual.Gender == Gender.Man ? individual : (TGenderedIndividual)individual.AsOtherGender();
+		TGenderedIndividual womanIndividual = individual.Gender == Gender.Woman ? individual : (TGenderedIndividual)individual.AsOtherGender();
+		return GetUnisexProbabilities(manIndividual, womanIndividual, calculationDate, dates, GetDecrementUnisexProbability, manProportion);
 	}
 	public decimal[] SurvivalUnisexProbabilities(TGenderedIndividual individual, in DateOnly calculationDate, OrderedDates dates, decimal manProportion)
 	{
@@ -116,7 +116,9 @@ public class GenderedDecrement<TGenderedIndividual> : Decrement<TGenderedIndivid
 			return GetProbabilities(individual.Gender == Gender.Man ? individual : (TGenderedIndividual)individual.AsOtherGender(), calculationDate, dates, GetSurvivalProbability);
 		if (manProportion == Decimal.Zero)
 			return GetProbabilities(individual.Gender == Gender.Woman ? individual : (TGenderedIndividual)individual.AsOtherGender(), calculationDate, dates, GetSurvivalProbability);
-		return GetUnisexProbabilities(individual, calculationDate, dates, GetSurvivalUnisexProbability, manProportion);
+		TGenderedIndividual manIndividual = individual.Gender == Gender.Man ? individual : (TGenderedIndividual)individual.AsOtherGender();
+		TGenderedIndividual womanIndividual = individual.Gender == Gender.Woman ? individual : (TGenderedIndividual)individual.AsOtherGender();
+		return GetUnisexProbabilities(manIndividual, womanIndividual, calculationDate, dates, GetSurvivalUnisexProbability, manProportion);
 	}
 	public decimal SurvivalUnisexProbability(TGenderedIndividual individual, in DateOnly calculationDate, in DateOnly decrementDate, decimal manProportion)
 	{
@@ -126,7 +128,9 @@ public class GenderedDecrement<TGenderedIndividual> : Decrement<TGenderedIndivid
 			return GetProbability(individual.Gender == Gender.Man ? individual : (TGenderedIndividual)individual.AsOtherGender(), calculationDate, decrementDate, GetSurvivalProbability);
 		if (manProportion == Decimal.Zero)
 			return GetProbability(individual.Gender == Gender.Woman ? individual : (TGenderedIndividual)individual.AsOtherGender(), calculationDate, decrementDate, GetSurvivalProbability);
-		return GetUnisexProbability(individual, calculationDate, decrementDate, GetSurvivalUnisexProbability, manProportion);
+		TGenderedIndividual manIndividual = individual.Gender == Gender.Man ? individual : (TGenderedIndividual)individual.AsOtherGender();
+		TGenderedIndividual womanIndividual = individual.Gender == Gender.Woman ? individual : (TGenderedIndividual)individual.AsOtherGender();
+		return GetUnisexProbability(manIndividual, womanIndividual, calculationDate, decrementDate, GetSurvivalUnisexProbability, manProportion);
 	}
 	#endregion
 }
